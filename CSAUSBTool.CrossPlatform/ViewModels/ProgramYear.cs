d6b69ce086a891f1f772a5cfac71454f1b0e8da2
Jamie Sinn <james.sinn@sinndevelopment.com>
2024-03-13 22:01:33 -0400

    UI Changes and refactor - working download on list per year
    need to actually setup the download functionality

--- CSAUSBTool.CrossPlatform/ViewModels/ProgramYear.cs
+++ CSAUSBTool.CrossPlatform/ViewModels/ProgramYear.cs
@@ -1,20 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json;
 using System.Threading.Tasks;
+using CSAUSBTool.CrossPlatform.Models;
+
 
 namespace CSAUSBTool.CrossPlatform.ViewModels
 {
     public class ProgramYear
     {
         public int Year { get; set; }
-        public string Program { get; set; } = "FRC";
+        public string Program { get; set; }
 
-        public List<ControlSystemSoftwareGroup> ControlSystemSoftwareGroups { get; set; } = new();
-        public ProgramYear()
+        public List<ControlSystemSoftwareGroup> SoftwareGroups { get; set; } = new();
+        public ProgramYear(int? year, string? program)
         {
+            year ??= DateTime.Now.Year;
+            program ??= "FRC";
+            Year = (int) year;
+            Program = program;
 
+            DownloadSoftwareLists();
+        }
+
+        private void DownloadSoftwareLists()
+        {
+            var downloadUrl =
+                $"https://raw.githubusercontent.com/JamieSinn/CSA-USB-Tool/avalonia/Lists/{Program}{Year}.json";
+
+            using var client = new HttpClient();
+            var software= client.GetFromJsonAsync<SeasonSoftwareList>(downloadUrl).Result;
+            
+                    //await client.GetStreamAsync(downloadUrl));
+            SoftwareGroups = software.GetGroups();
         }
     }
 }
